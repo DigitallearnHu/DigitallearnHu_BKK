7504fef449ea161fb7cb4db6083e2724940f6db0
DigitallearnHu <digitallearn.hu@gmail.com>
2025-05-30 16:13:06 +0200

    Update sheet_manager.py

--- sheet_manager.py
+++ sheet_manager.py
@@ -14,7 +14,7 @@ def get_sheet():
     json_data = json.loads(st.secrets["SERVICE_ACCOUNT_JSON"])
     creds = ServiceAccountCredentials.from_json_keyfile_dict(json_data, scope)
     client = gspread.authorize(creds)
-    sheet = client.open("Digitallearn_BKK").worksheet("users")
+    sheet = client.open_by_key("1BcOHJxaSAh5uGrS9y0jEMoKDlEDUXHfWtPIQGaXH1U4").worksheet("Datas")
     return sheet
 
 def hash_password(password):
